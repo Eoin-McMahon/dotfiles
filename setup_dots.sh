15eee450a3306a8764ab499c6940420a1e0d8fd0
Eoin McMahon <eoin.mcmahon.dev@gmail.com>
2021-06-08 23:37:15 +0200

    add setup script

---
+++ setup_dots.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Running Setup..."
+
+# Clones bare repo and also a temp normal repo which contains the working tree
+git clone \
+    --separate-git-dir=$HOME/dotfiles \
+    https://github.com/Eoin-McMahon/dotfiles.git \
+    $HOME/dotfiles-TEMP
+
+# Copies files from temp repo to their respective locations on your pc
+rsync --recursive --verbose --exclude '.git' --exclude 'README.md' $HOME/dotfiles-TEMP/ $HOME/
+
+# remove the temp repo
+rm -rf $HOME/dotfiles-TEMP
+
+# dont show untracked files
+/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
+
+# use config alias instead of git command when working with config files
+echo "alias config=\"/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME/\"" >> $HOME/.bashrc
+
